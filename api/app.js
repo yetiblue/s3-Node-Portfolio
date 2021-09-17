e6705a02e60346181f249a4c17ce6eab7739ef7d
yetiblue <thempver@gmail.com>
2021-09-17 11:59:41 -0400

    add: for-loop for array of photos working

--- api/app.js
+++ api/app.js
@@ -1,27 +1,23 @@
-// const AWS = require("aws-sdk");
 const dotenv = require("dotenv");
 dotenv.config();
 const ID = `${process.env.ID}`;
+console.log(ID);
 const SECRET = `${process.env.SECRET}`;
 const BUCKET_NAME = `${process.env.BUCKET_NAME}`;
 const fs = require("fs");
 const AWS = require("aws-sdk");
+const photoArray = ["test1.png", "test2.png", "test3.png", "test4.png"];
 
 const s3 = new AWS.S3({
   accessKeyId: ID,
   secretAccessKey: SECRET,
 });
-// const params = {
-//   Bucket: BUCKET_NAME,
-//   CreateBucketConfiguration: {
-//     LocationConstraint: "us-east-2",
-//   },
-// };
+
 const uploadFile = (fileName) => {
   const fileContent = fs.readFileSync(fileName);
   const params = {
     Bucket: BUCKET_NAME,
-    Key: "test.jpg",
+    Key: fileName,
     Body: fileContent,
   };
   s3.upload(params, function(err, data) {
@@ -31,8 +27,7 @@ const uploadFile = (fileName) => {
     console.log(`File uploaded successfully. ${data.Location}`);
   });
 };
-uploadFile("test.png");
-// s3.createBucket(params, function(err, data) {
-//   if (err) console.log(err, err.stack);
-//   else console.log("Bucket Created Successfully", data.Location);
-// });
+for (let i = 0; i < photoArray.length; i++) {
+  uploadFile(photoArray[i]);
+  console.log("uploaded", photoArray[i]);
+}
