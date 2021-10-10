88253ccae02f8af2eb2ef50c97d17ac927bd2ff1
yetiblue <thempver@gmail.com>
2021-10-10 16:58:19 -0400

    i really really really really really hate CSS

--- client/src/stateless/header.js
+++ client/src/stateless/header.js
@@ -6,32 +6,33 @@ function Header() {
   return (
     <div className="wrapper">
       <MetaTags>
-        {/* {" "} */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </MetaTags>
-      <div className="header col-mobile-12">
-        <div className="col-6 col-mobile-6 header__site-title">
+      <div className="header col-12">
+        <div className="col-6 header__site-title">
           <h1>Timmy Zhou</h1>
         </div>
+        <div className="hamburger col-0">
+          <button>☰ </button>
+        </div>
 
-        <nav className="navbar col-mobile-1">
-          <ul className="navbar_list ">
-            <li>
-              <a className="navbar_link" href="">
-                Home
-              </a>
-              <a className="navbar_link" href="">
-                Blog
-              </a>
-              <a className="navbar_link" href="">
-                About
-              </a>
-            </li>
+        <div className="navbar col-5">
+          <ul className="navbar_list">
+            <a className="navbar_link" href="">
+              Home
+            </a>
+
+            <a className="navbar_link" href="">
+              Blog
+            </a>
+
+            <a className="navbar_link" href="">
+              About
+            </a>
           </ul>
-        </nav>
-        {/* <nav className="navbar">
-          <ul className="navbar_list col-mobile-11">BOo</ul>
-        </nav> */}
+        </div>
+
+        {/* <button className="openbtn">☰ Open Sidebar</button> */}
       </div>
     </div>
   );
