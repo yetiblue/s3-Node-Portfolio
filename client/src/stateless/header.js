89a66d8081ed03b271914fa835d3076236327f15
yetiblue <thempver@gmail.com>
2021-10-10 13:35:53 -0400

    navbar work

--- client/src/stateless/header.js
+++ client/src/stateless/header.js
@@ -1,24 +1,38 @@
 // import logo from "./logo.svg";
 import "./header.css";
+import MetaTags from "react-meta-tags";
 
 function Header() {
   return (
-    <div class="header">
-      <nav class="navbar">
-        <ul class="navbar_list">
-          <li>
-            <a class="navbar_link" href="">
-              Home
-            </a>
-            <a class="navbar_link" href="">
-              Blog
-            </a>
-            <a class="navbar_link" href="">
-              About
-            </a>
-          </li>
-        </ul>
-      </nav>
+    <div className="wrapper">
+      <MetaTags>
+        {/* {" "} */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </MetaTags>
+      <div className="header col-mobile-12">
+        <div className="col-6 col-mobile-6 header__site-title">
+          <h1>Timmy Zhou</h1>
+        </div>
+
+        <nav className="navbar col-mobile-1">
+          <ul className="navbar_list ">
+            <li>
+              <a className="navbar_link" href="">
+                Home
+              </a>
+              <a className="navbar_link" href="">
+                Blog
+              </a>
+              <a className="navbar_link" href="">
+                About
+              </a>
+            </li>
+          </ul>
+        </nav>
+        {/* <nav className="navbar">
+          <ul className="navbar_list col-mobile-11">BOo</ul>
+        </nav> */}
+      </div>
     </div>
   );
 }
