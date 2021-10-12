01e45ab19881a29b6818682e9a4e3973c5cd1ac5
yetiblue <thempver@gmail.com>
2021-10-12 10:44:14 -0400

    sidebar progress

--- client/src/stateless/header.js
+++ client/src/stateless/header.js
@@ -1,41 +1,142 @@
 // import logo from "./logo.svg";
 import "./header.css";
 import MetaTags from "react-meta-tags";
+import React from "react";
 
-function Header() {
-  return (
-    <div className="wrapper">
-      <MetaTags>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </MetaTags>
-      <div className="header col-12">
-        <div className="header__site-title">
-          <h1>Timmy Zhou</h1>
-        </div>
-        <div className="hamburger">
-          <button className="drawerBtn">☰ </button>
-        </div>
-
-        <div className="navbar">
-          <ul className="navbar_list">
-            <a className="navbar_link" href="">
-              Home
-            </a>
-
-            <a className="navbar_link" href="">
-              Blog
-            </a>
-
-            <a className="navbar_link" href="">
-              About
-            </a>
+class Header extends React.Component {
+  constructor() {
+    super();
+    this.state = { showSidebar: false };
+  }
+  openSidebar() {
+    this.setState({ showSidebar: !this.state.showSidebar });
+  }
+  render() {
+    let sidebar = this.state.showSidebar ? "sidebar" : "sidebar-show";
+    return (
+      <div className="wrapper">
+        <MetaTags>
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </MetaTags>
+        <div className="sidebar sidebar-show">
+          <ul className="sidebar__ul">
+            <li className="sidebar__li">
+              <button
+                // onClick={this.showSidebar.bind(this)}
+                className="sidebar__button"
+              >
+                X
+              </button>
+            </li>
+            <li className="sidebar__li">
+              <a className="sidebar__link" href="">
+                Home
+              </a>
+            </li>
+            <li className="sidebar__li">
+              <a className="sidebar__link" href="">
+                Blog
+              </a>
+            </li>
+            <li className="sidebar__li">
+              <a className="sidebar__link" href="">
+                About
+              </a>
+            </li>
           </ul>
         </div>
+        <div className="header col-12">
+          <div className="header__site-title">
+            {/* <h1>Timmy Zhou</h1> */}
+            <h1>Sample Text</h1>
+          </div>
+          <div className="hamburger">
+            <button className="drawerBtn">☰ </button>
+          </div>
 
-        {/* <button className="openbtn">☰ Open Sidebar</button> */}
+          <div className="navbar">
+            <ul className="navbar__list">
+              <a className="navbar__link" href="">
+                Home
+              </a>
+
+              <a className="navbar__link" href="">
+                Blog
+              </a>
+
+              <a className="navbar__link" href="">
+                About
+              </a>
+            </ul>
+          </div>
+
+          {/* <button className="openbtn">☰ Open Sidebar</button> */}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+// function Header() {
+//   return (
+//     <div className="wrapper">
+//       <MetaTags>
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+//       </MetaTags>
+//       <div className="sidebar sidebar-show">
+//         <ul className="sidebar__ul">
+//           <li className="sidebar__li">
+//             <button className="sidebar__button"> X</button>
+//           </li>
+//           <li className="sidebar__li">
+//             <a className="sidebar__link" href="">
+//               Home
+//             </a>
+//           </li>
+//           <li className="sidebar__li">
+//             <a className="sidebar__link" href="">
+//               Blog
+//             </a>
+//           </li>
+//           <li className="sidebar__li">
+//             <a className="sidebar__link" href="">
+//               About
+//             </a>
+//           </li>
+//         </ul>
+//       </div>
+//       <div className="header col-12">
+//         <div className="header__site-title">
+//           {/* <h1>Timmy Zhou</h1> */}
+//           <h1>Sample Text</h1>
+//         </div>
+//         <div className="hamburger">
+//           <button className="drawerBtn">☰ </button>
+//         </div>
+
+//         <div className="navbar">
+//           <ul className="navbar__list">
+//             <a className="navbar__link" href="">
+//               Home
+//             </a>
+
+//             <a className="navbar__link" href="">
+//               Blog
+//             </a>
+
+//             <a className="navbar__link" href="">
+//               About
+//             </a>
+//           </ul>
+//         </div>
+
+//         {/* <button className="openbtn">☰ Open Sidebar</button> */}
+//       </div>
+//     </div>
+//   );
+// }
 
 export default Header;
