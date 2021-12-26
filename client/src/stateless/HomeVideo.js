da5d1c930912956c82022532de6ca59162758f20
yetiblue <thempver@gmail.com>
2021-12-26 17:24:12 -0500

    home page done

--- client/src/stateless/HomeVideo.js
+++ client/src/stateless/HomeVideo.js
@@ -1,30 +1,42 @@
 import "./HomeVideo.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  withRouter,
+} from "react-router-dom";
+
 function HomeVideo() {
   return (
-    <div>
-      {/* <Link
-              className="logo_nav"
-              style={{ textDecoration: "none", color: "black" }}
-              to="/"
-          > */}
-
-      {/* </Link> */}
-
-      <video autoPlay muted loop>
+    <div className="home-content">
+      {/* <div className="videoContainer"> */}
+      <video className="home-video" autoPlay muted loop>
         <source
-          className="home-video"
           src="https://timmyportfolio.s3.us-east-2.amazonaws.com/videos/background2.mov"
           type="video/mp4"
         ></source>
-        Your browser does not support the video tag.
       </video>
+      {/* </div> */}
       <div className="header__site-title-home">
-        <h1>Timmy Zhou</h1>
+        <h1 className="home__name">Timmy Zhou</h1>
       </div>
       <ul className="bottom-nav">
-        <li className="bottom-nav__link">About</li>
-        <li className="bottom-nav__link">Generative</li>
-        <li className="bottom-nav__link">Photography</li>
+        <li className="bottom-nav__link">
+          <Link className="bottom-nav__link__first" to="/about">
+            About
+          </Link>
+        </li>
+        <li className="bottom-nav__link">
+          <Link className="bottom-nav__link" to="/generative">
+            Generative{" "}
+          </Link>
+        </li>
+        <li className="bottom-nav__link">
+          <Link className="bottom-nav__link" to="/photography">
+            Photography
+          </Link>
+        </li>
       </ul>
     </div>
   );
