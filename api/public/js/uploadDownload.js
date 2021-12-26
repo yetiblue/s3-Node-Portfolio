0ed5007296d18bb579b12d2ee6f337b38f8a6f0a
yetiblue <thempver@gmail.com>
2021-12-26 18:52:53 -0500

    checkpoint for filtering out duplicates when fetching from S3

--- api/public/js/uploadDownload.js
+++ api/public/js/uploadDownload.js
@@ -2,7 +2,10 @@ import fs from "fs";
 import * as constants from "./constants.js";
 
 let photoArray = []; //holding downoaded photos from the s3 bucket
-
+let previousArray = [];
+let searchGenre; // use in retrieving from Mongo to filter out duplicates.
+let emptyDBEdgeCase = true; //the first time things run, use the photoArray.
+//after that use filterResult
 async function createObject(client, newObject) {
   //send photo src to MongoDB
   const result = await client
@@ -16,11 +19,29 @@ async function main() {
   try {
     await constants.client.connect();
     console.log("main connected");
-    photoArray.splice(0, 1); //the uri for the object/folder instead of the image is included as the first item
-    for (let i = 0; i < photoArray.length; i++) {
-      console.log(photoArray[i]);
-      console.log(photoArray, "full photoarray");
-      await createObject(constants.client, photoArray[i]);
+    previousArray = await constants.client
+      .db("portfolio_images")
+      .collection("images")
+      .find({ genre: searchGenre })
+      .toArray();
+    console.log(previousArray, "after searching");
+    console.log(photoArray, "photoarray"); //should have new images
+    console.log(previousArray, "previous array"); //should have old images
+    const filterResult = photoArray.filter((x) => !previousArray.includes(x)); //should return new images that are in photoArray and not in previousArray
+    console.log(filterResult, "filterResult");
+    if (emptyDBEdgeCase) {
+      for (let i = 0; i < photoArray.length; i++) {
+        // console.log(photoArray[i]);
+        // console.log(photoArray, "full photoarray");
+        await createObject(constants.client, photoArray[i]);
+        emptyDBEdgeCase = false;
+      }
+    } else {
+      for (let i = 0; i < filterResult.length; i++) {
+        // console.log(photoArray[i]);
+        // console.log(photoArray, "full photoarray");
+        await createObject(constants.client, filterResult[i]);
+      }
     }
   } catch (e) {
     console.log(e);
@@ -77,16 +98,24 @@ export function viewAlbum(albumName) {
 
     var photos = data.Contents.map(function(photo) {
       var photoKey = photo.Key; //name of the file in 'data'
+
       var photoUrl = newBucketUrl + encodeURIComponent(photoKey);
-      let incrementID = 0;
-      photoArray.push({
-        src: photoUrl,
-        genre: albumName,
-      }); //include param from above that gets filled with the collection name which would be added to tag: key
-      // console.log(photoArray, ": photoarray");
-      photoArray.forEach((photo, incrementID) => {
-        photo.id = incrementID += 1;
-      });
+      if (
+        photoUrl ==
+        `https://timmyportfolio.s3.us-east-2.amazonaws.com/${albumName}%2F`
+      ) {
+      } else {
+        searchGenre = albumName;
+        let incrementID = 0;
+        photoArray.push({
+          src: photoUrl,
+          genre: albumName,
+        }); //include param from above that gets filled with the collection name which would be added to tag: key
+        // console.log(photoArray, ": photoarray");
+        photoArray.forEach((photo, incrementID) => {
+          photo.id = incrementID += 1;
+        });
+      }
     });
 
     main().catch(console.error);
