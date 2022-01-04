1e89f21b87504f644b761bd89ce8ae09b09ae87d
yetiblue <thempver@gmail.com>
2022-01-04 18:16:22 -0500

    about page positioning

--- client/src/stateless/PhotoMenu.js
+++ client/src/stateless/PhotoMenu.js
@@ -13,28 +13,27 @@ import {
 function PhotoMenu(props) {
   useEffect(() => {
     //using hook to retrieve the '/generative' part, or else on first load, window.location.pathname wont be availble.
-
-    if (window.location.pathname == "/generative" && window.innerWidth > 1312) {
-      document.getElementsByClassName("menuWrapper")[0].style.marginLeft =
-        "23%"; //on desktop, bc there's only 2 squares instead of 4 they need to be in the center of the page
-      console.log(window.innerWidth, "width");
-    } else if (
-      window.location.pathname == "/generative" &&
-      window.innerWidth > 312 &&
-      window.innerWidth < 767
-    ) {
-      document.getElementsByClassName("menuWrapper")[0].style.marginLeft =
-        "-7%"; //on mobile all the otions are stacked, so this just needs a bit of nudging to the left
-      console.log(window.innerWidth, "width");
-    } else if (
-      window.location.pathname == "/generative" &&
-      window.innerWidth > 768 &&
-      window.innerWidth < 1024
-    ) {
-      document.getElementsByClassName("menuWrapper")[0].style.marginLeft =
-        "23%";
-      document.getElementsByClassName("menuWrapper")[0].style.marginTop = "18%";
-    }
+    // if (window.location.pathname == "/generative" && window.innerWidth > 1312) {
+    //   document.getElementsByClassName(
+    //     "photoWrapper__li-menu"
+    //   )[0].style.marginLeft = "10em"; //on desktop, bc there's only 2 squares instead of 4 they need to be in the center of the page
+    //   console.log(window.innerWidth, "width");
+    // }
+    //   window.location.pathname == "/generative" &&
+    //   window.innerWidth > 312 &&
+    //   window.innerWidth < 767
+    // ) {
+    //   document.getElementsByClassName("menuWrapper")[0].style.marginLeft =
+    //     "-7%"; //on mobile all the otions are stacked, so this just needs a bit of nudging to the left
+    //   console.log(window.innerWidth, "width");
+    // } else if (
+    //   window.location.pathname == "/generative" &&
+    //   window.innerWidth > 768 &&
+    //   window.innerWidth < 1024
+    // ) {
+    //   document.getElementsByClassName("menuWrapper")[0].style.marginLeft = "0%";
+    //   document.getElementsByClassName("menuWrapper")[0].style.marginTop = "18%";
+    // }
   }, []);
 
   const listPhotos = props.photoList.map((photo) => (
